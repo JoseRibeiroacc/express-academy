a8c57ce2b8c87233e0c4341cb07bcd014c14d860
JoseRibeiroacc <jose.miranda.ribeiro@accenture.com>
2026-03-05 17:00:49 +0000

    Initial project setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const app = express();
+
+
+app.get('/', (request, response) => {
+    response.send('<h1> Hello World! </h1>')
+})
+
+app.listen(config.PORT,() => {
+    logger.info(`Server running on port ${config.PORT}`)
+})
